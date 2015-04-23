2fcb0c907702fb94a29461a445374a0e98a08e75
Thomas Stringer <sqlsalt@outlook.com>
2015-04-23 16:16:04 -0400

    Added logic to validate release date
    Used regex to validate and take correct action based off the input release
    date.

--- SqlServerVersions/Scripts/site2.js
+++ SqlServerVersions/Scripts/site2.js
@@ -1,9 +1,33 @@
-﻿$(document).ready(function () {
+﻿function isDateValid(dateInput) {
+    var dateRegex = new RegExp(/\b\d{1,2}[\/-]\d{1,2}[\/-]\d{4}\b/);
+    return dateRegex.test(dateInput);
+}
+
+$(document).ready(function () {
     $("#input-friendlynamelong").keyup(function () {
         $("#repeat-friendlynamelong").text($(this).val());
     });
 
     $("#input-friendlynameshort").keyup(function () {
         $("#repeat-friendlynameshort").text($(this).val());
+    });
+
+    $("#input-releasedate").change(function () {
+        var releaseDateInput = $(this).val();
+
+        if (!releaseDateInput.trim()) {
+            $("#repeat-releasedate").text("");
+            $(".date-error").css("visibility", "hidden");
+        }
+        else {
+            if (isDateValid(releaseDateInput)) {
+                $(".date-error").css("visibility", "hidden");
+                $("#repeat-releasedate").text(releaseDateInput);
+            }
+            else {
+                $("#repeat-releasedate").text("");
+                $(".date-error").css("visibility", "visible");
+            }
+        }
     });
 });
